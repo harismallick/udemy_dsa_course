b0b75fcb0ca7fa8dd471ffed11dd90098cbdb4f6
Haris Mallick <mallick.haris@gmail.com>
2026-02-23 03:01:36 +0000

    On lesson 211

--- 11_section/11_notes.md
+++ 11_section/11_notes.md
@@ -16,3 +16,39 @@
 - But, searching through a linked list can be optimised by using transposition or move-to-head methods learned previously.
 - In move to head, the node being searched will be made the head of the LL.
 - If the same value is searched again, then the operation would be done in O(1) time.
+
+#### Lesson 198
+
+- Reversing the elements in the nodes rather than the pointers pointing to each node.
+- Not an optimal solution as you need to create an array the same size as the linked list to hold all the values.
+- Time complexity: O(2n) ~ O(n)
+
+```
+Pseudocode
+
+p = first_node; //pointer_to_node
+array[n]; // n elements in the linked list
+int i = 0;
+
+while (p != NULL)
+    copy data from node at p to array[i];
+    i++;
+    p = p->next;
+
+p = first_node
+
+while (p != NULL)
+    copy data from array[i] to p->data;
+    i--;
+    p = p->next;
+```
+
+#### Lesson 199
+
+- Reversing the pointers connecting the linked list to reverse the list.
+- This operation can be done by looping through the linked list only **once**.
+- **Three pointers** needed to solve the problem.
+- One pointer to point to the current node.
+- One to point to the previous node.
+- One to point to the next node.
+
--- 11_section/linked-list.c
+++ 11_section/linked-list.c
@@ -38,6 +38,15 @@ void ll_delete_node_by_index(LinkedList* list, int index);
 bool ll_is_sorted(LinkedList* list);
 bool ll_is_sorted_ascending(LinkedList* list);
 bool ll_is_sorted_descending(LinkedList* list);
+void ll_delete_duplicate_nodes(LinkedList* list);
+void ll_reverse(LinkedList* list);
+void ll_reverse_recursive(LinkedList* list, Node* prev, Node* curr);
+void ll_concatenate(LinkedList* list1, LinkedList* list2);
+LinkedList* ll_sorted_merge(LinkedList* list1, LinkedList* list2);
+bool ll_is_cyclic(LinkedList* list);
+void circular_ll_display(LinkedList* list);
+void circular_ll_recursive_display(LinkedList* list, Node* p);
+void ll_circular_free_by_address(LinkedList* list);
 
 int main()
 {
@@ -128,9 +137,89 @@ int main()
     ll_add(test_three, 20);
     ll_is_sorted(test_three) ? printf("List is sorted.\n") : printf("List is not sorted.\n");
 
+    // lesson 196
+    LinkedList* lesson_196 = ll_create();
+    int arr3[] = {3,5,5,8,8,8};
+    ll_add_iter(lesson_196, arr3, sizeof(arr3)/sizeof(int));
+    ll_display(lesson_196);
+    ll_delete_duplicate_nodes(lesson_196);
+    ll_display(lesson_196);
+
+    // lesson 199
+    printf("test_two before reversing:\n");
+    ll_display(test_two);
+    ll_reverse(test_two);
+    printf("test_two after reversing:\n");
+    ll_display(test_two);
+    ll_reverse_recursive(test_two, NULL, test_two->head);
+    printf("test_two after recursive reversing:\n");
+    ll_display(test_two);
+
+    // lesson 202
+    ll_concatenate(test_two, lesson_196);
+    printf("test_two list after concatenation:\n");
+    ll_display(test_two);
+
+    // lesson 203
+    LinkedList* l1 = ll_create();
+    int l1_arr[] = {1,3,5,7};
+    ll_add_iter(l1, l1_arr, 4);
+
+    LinkedList* l2 = ll_create();
+    int l2_arr[] = {2,4,6,8,9,10};
+    ll_add_iter(l2, l2_arr, 6);
+
+    LinkedList* merged = ll_sorted_merge(l1, l2);
+    ll_display(merged);
+
+    // lesson 205
+    // test 1
+    bool is_cyclic = ll_is_cyclic(merged);
+    if (is_cyclic)
+    {
+        printf("The given linked list is cyclic.\n");
+    }
+    else
+    {
+        printf("The given linked list is not cyclic.\n");
+    }
+
+    // test 2
+    Node n1 = {1, NULL};
+    Node n2 = {2, NULL};
+    Node n3 = {3, NULL};
+    Node n4 = {4, NULL};
+    n1.next = &n2;
+    n2.next = &n3;
+    n3.next = &n4;
+    n4.next = &n1;
+    LinkedList* cyclic_ll = ll_create();
+    cyclic_ll->head = &n1;
+    cyclic_ll->node_count = 4;
+
+    is_cyclic = ll_is_cyclic(cyclic_ll);
+    if (is_cyclic)
+    {
+        printf("The given linked list is cyclic.\n");
+    }
+    else
+    {
+        printf("The given linked list is not cyclic.\n");
+    }
+
+    // lesson 209
+    circular_ll_display(cyclic_ll);
+    circular_ll_recursive_display(cyclic_ll, cyclic_ll->head);
+
     ll_free(test_list);
     ll_free(test_two);
     ll_free(test_three);
+    ll_free(lesson_196);
+    ll_free(l1);
+    ll_free(l2);
+    ll_free(merged);
+    // ll_circular_free_by_address(cyclic_ll);
+
 
     return 0;
 }
@@ -546,4 +635,210 @@ bool ll_is_sorted_descending(LinkedList* list)
         temp = temp->next;
     }
     return true;
+}
+
+void ll_delete_duplicate_nodes(LinkedList* list)
+{
+    Node *first, *second;
+    if (!list->head)
+    {
+        printf("Empty linked list\n");
+        return;
+    }
+
+    first = list->head;
+    second = first->next;
+
+    while (second != NULL)
+    {
+        if (first->data != second->data)
+        {
+            first = second;
+            second = second->next;
+        }
+        else
+        {
+            first->next = second->next;
+            // second is now an orphaned node
+            free(second);
+            second = first->next;
+            list->node_count--;
+        }
+    }
+}
+
+void ll_reverse(LinkedList* list)
+{
+    if (!list->head || !list->head->next)
+    {
+        printf("Not enough elements to reverse the list.\n");
+    }
+
+    // three pointer solution
+    Node *prev, *curr, *next;
+    prev = NULL;
+    curr = list->head;
+    next = NULL;
+
+    while (curr != NULL)
+    {
+        next = curr->next;
+
+        curr->next = prev;
+        // next->next = curr;
+
+        prev = curr;
+        curr = next;
+    }
+    list->head = prev;
+}
+
+void ll_reverse_recursive(LinkedList* list, Node* prev, Node* curr)
+{
+    if (curr == NULL)
+    {
+        list->head = prev;
+        return;
+    }
+
+    ll_reverse_recursive(list, curr, curr->next);
+    curr->next = prev;
+}
+
+void ll_concatenate(LinkedList* list1, LinkedList* list2)
+{
+    Node* current;
+    current = list1->head;
+
+    while (current->next != NULL)
+    {
+        current = current->next;
+    }
+    current->next = list2->head;
+    // free(list2); // Only if required
+}
+
+LinkedList* ll_sorted_merge(LinkedList* list1, LinkedList* list2)
+{
+    // Merging two sorted lists while maintaining the sorted order
+    Node *l1_p, *l2_p, *merged_p;
+    l1_p = list1->head;
+    l2_p = list2->head;
+    merged_p = NULL;
+
+    LinkedList* merged = (LinkedList*)malloc(sizeof(LinkedList));
+    merged->head = merged_p;
+    merged->node_count = list1->node_count + list2->node_count;
+
+    while (l1_p && l2_p)
+    {
+        if (l1_p->data <= l2_p->data)
+        {
+            if (!merged->head)
+            {
+                merged->head = merged_p = l1_p;
+            }
+            else
+            {
+                merged_p->next = l1_p;
+                merged_p = merged_p->next;
+            }
+            l1_p = l1_p->next;
+        }
+        else
+        {
+            if (!merged->head)
+            {
+                merged->head = merged_p = l2_p;
+            }
+            else
+            {
+                merged_p->next = l2_p;
+                merged_p = merged_p->next;
+            }
+            l2_p = l2_p->next;
+
+        }
+
+    }
+    if (l1_p)
+    {
+        merged_p->next = l1_p;
+    }
+    if (l2_p)
+    {
+        merged_p->next = l2_p;
+    }
+    return merged;
+}
+
+bool ll_is_cyclic(LinkedList* list)
+{
+    Node *p1, *p2;
+    p1 = p2 = list->head;
+
+    do
+    {
+        if (!p1 || !p2)
+        {
+            return false;
+        }
+
+        p1 = p1->next;
+        p2 = p2->next->next;
+    } while (p1 != p2);
+    
+    return true;
+}
+
+void circular_ll_display(LinkedList* list)
+{
+    Node* p = list->head;
+
+    do
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    } while (p && p != list->head);
+    printf("\n");
+}
+
+void ll_circular_free_by_address(LinkedList* list) {
+    if (list == NULL || list->head == NULL) {
+        if (list) free(list);
+        return;
+    }
+
+    Node* head = list->head;
+    Node* current = head;
+    Node* next_node;
+
+    // We use a 'do-while' or manual break to ensure 
+    // we process the first node before checking the condition
+    do {
+        next_node = current->next;
+        free(current);
+        current = next_node;
+    } while (current != head);
+
+    list->head = NULL;
+    list->node_count = 0;
+    free(list);
+}
+
+void circular_ll_recursive_display(LinkedList* list, Node* p)
+{
+    static int flag = 0;
+
+    if (p != list->head || flag == 0)
+    {
+        flag = 1;
+        printf("%d ", p->data);
+        circular_ll_recursive_display(list, p->next);
+    }
+    else
+    {
+        printf("\n");
+    }
+    flag = 0;
 }
